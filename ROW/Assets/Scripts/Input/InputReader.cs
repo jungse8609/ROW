df25cbee69c07dd11dbfcae43f514350d86153ff
jungse8609 <69056797+jungse8609@users.noreply.github.com>
2024-12-12 20:00:38 +0900

    feat: Add Player movement Action
    - add package : cinemachine, input system

---
+++ ROW/Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader", order = 0)]
+public class InputReader : ScriptableObject, GameInput.IGameplayActions
+{
+    // Gameplay
+    public event UnityAction<Vector2> MoveEvent = delegate { };
+    public event UnityAction FireEvent = delegate { };
+    public event UnityAction ReloadEvent = delegate { };
+
+    // Menues
+
+    private GameInput _gameInput;
+
+    private void OnEnable()
+    {
+        if (_gameInput == null)
+        {
+            _gameInput = new GameInput();
+
+            _gameInput.Gameplay.SetCallbacks(this);
+            _gameInput.Gameplay.Enable();
+        }
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        MoveEvent.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            FireEvent.Invoke();
+    }
+
+    public void OnReload(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            ReloadEvent.Invoke();
+    }
+}
